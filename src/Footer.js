aeb863e6e9d283a515d085caf91daf3458729382
Tomaž <tomanclin@gmail.com>
2023-09-08 07:43:02 +0200

    finished the basic components setup and add logo2

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,19 @@
+import logo2 from "./assets/logo2.png";
+
 function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <img src={logo2} alt="lemon" />
+      <h2>Doormat Navigation</h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+    </footer>
+  );
 }
 
 export default Footer;
