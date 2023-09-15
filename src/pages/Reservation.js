4c04a00c24425b6ec24b6f04571cbfc5696cc342
Tomaž <tomanclin@gmail.com>
2023-09-15 07:43:58 +0200

    added buttons in reservations

--- src/pages/Reservation.js
+++ src/pages/Reservation.js
@@ -12,6 +12,36 @@ function Reservation() {
       }}
     >
       <h1>Reservation</h1>
+      <div style={{ display: "flex", gap: "50px" }}>
+        <button
+          style={{
+            backgroundColor: "#F4CE14",
+            border: "none",
+            width: "300px",
+            height: "80px",
+            fontSize: "2em",
+            borderRadius: "12px",
+            fontWeight: "bold",
+            fontFamily: "Karla",
+          }}
+        >
+          Book a table
+        </button>
+        <button
+          style={{
+            backgroundColor: "#F4CE14",
+            border: "none",
+            width: "300px",
+            height: "80px",
+            fontSize: "2em",
+            borderRadius: "12px",
+            fontWeight: "bold",
+            fontFamily: "Karla",
+          }}
+        >
+          Cancel
+        </button>
+      </div>
     </div>
   );
 }
